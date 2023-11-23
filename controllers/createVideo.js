ad243fb5e5dbe304e600c6ab8da872b33149fd24
fabian <godsonhm3@gmail.com>
2023-11-23 21:41:13 +0100

    Refector video controller to not save video to DB

--- controllers/createVideo.js
+++ controllers/createVideo.js
@@ -14,9 +14,7 @@ const createVideo = async (req, res) => {
       .json({ error: "No file uploaded" });
   }
 
-  // Process the file (e.g., save to disk, send in API response)
-
-  // Save the file to disk (adjust the path as needed)
+  // Save the file to upload folder
   const filePath = `uploads/${uploadedFile.originalname}`;
   fs.writeFileSync(filePath, uploadedFile.buffer);
 
@@ -27,24 +25,31 @@ const createVideo = async (req, res) => {
     folder: "screenTalk_videos",
   });
 
-  if (result.duration > 300.0) {
+  if (result.duration > 200.0) {
     throw new BadRequestError(
-      "Sorry, we can't proccess videos that are longer than 5 minutes"
+      "Sorry, we can't proccess videos that's longer than 3 minutes"
     );
   }
 
-  // Save the video information to DB
-  const newVideo = new Video({
-    name: req.body.name,
-    video: result.secure_url, // Save the file path or URL to the video field
-    ip: req.body.ip,
-    videoDuration: result.duration,
-  });
-  await newVideo.save();
-  // Send a response to the client
+  // Function to delete the video from Cloudinary
+  const deleteVideoFromCloudinary = () => {
+    cloudinary.uploader.destroy(filePath, (error, result) => {
+      if (error) {
+        console.error("Error deleting video from Cloudinary:", error);
+      } else {
+        console.log(
+          "Video deleted from Cloudinary. Cloudinary response:",
+          result
+        );
+      }
+    });
+  };
+  // Schedule the deletion after 20 minutes
+  const deletionTimeout = 20 * 60 * 1000;
+  // setTimeout(deleteVideoFromCloudinary, deletionTimeout);
   res
     .status(StatusCodes.CREATED)
-    .json({ message: "File uploaded and processed successfully" });
+    .json({ video: result.secure_url, duration: result.duration });
 };
 
 module.exports = { createVideo };
