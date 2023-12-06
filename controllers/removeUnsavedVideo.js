7b8a9d6f853833f267a646216017d5f518ac88c0
fabian <godsonhm3@gmail.com>
2023-12-06 13:59:15 +0100

    schedule delete unsaved video

--- controllers/removeUnsavedVideo.js
+++ controllers/removeUnsavedVideo.js
@@ -1,52 +1,33 @@
 const { BadRequestError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 const cloudinary = require("cloudinary").v2;
-const cron = require("node-cron");
-// In-memory cache to track scheduled deletions
-const scheduledDeletions = new Map();
+const schedule = require("node-schedule");
 
 const deleteVideoFromServer = async (req, res) => {
-  // Send "ok" response immediately
-  // res.status(200).json("ok");
+  // function to delete a video by public_id
+  const deleteVideo = (public_id) => {
+    cloudinary.uploader.destroy(
+      public_id,
+      {
+        resource_type: "video",
+      },
+      (error, result) => {
+        if (error) {
+          console.error("Error deleting video:", error);
+        } else {
+          console.log("Video deleted successfully:", result);
+        }
+      }
+    );
+  };
 
-  // // Generate a unique request token for tracking
-  // const requestToken = `${Date.now()}_${Math.random()}`;
-
-  // // Schedule the deletion using node-cron (every 30 minutes)
-  // const cronJob = cron.schedule(
-  //   "0 */1 * * *",
-  //   async () => {
-  //     try {
-  //       // Delete the video
-  //       const publicId = req.body.data;
-  //       const result = await cloudinary.uploader.destroy(publicId, {
-  //         resource_type: "video",
-  //       });
-  //       console.log("Video deleted:", result);
-  //     } catch (error) {
-  //       console.error("Error deleting video:", error);
-  //     } finally {
-  //       // Remove the request token and stop the cron job after deletion
-  //       scheduledDeletions.delete(requestToken);
-  //       cronJob.stop();
-  //     }
-  //   },
-  //   {
-  //     scheduled: false, // Start the job immediately
-  //     timezone: "UTC",
-  //   }
-  // );
-
-  // // Start the cron job
-  // cronJob.start();
-  // scheduledDeletions.set(requestToken, cronJob);
-  // //////////////////////////////////////////
-  const publicId = req.body.data;
-  const result = await cloudinary.uploader.destroy(publicId, {
-    resource_type: "video",
+  const id = req.body.data;
+  // Schedule the video deletion for 3 minutes from now
+  const scheduledTime = new Date(Date.now() + 3 * 60 * 1000);
+  schedule.scheduleJob(scheduledTime, () => {
+    deleteVideo(id);
   });
-  // console.log("Video deleted:", result);
-  res.status(200).json("ok");
+  res.status(200).json("video deletion scheduled successfully");
 };
 
 module.exports = { deleteVideoFromServer };
