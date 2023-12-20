b7023a64031157fbcebf84f03e7bb1715ea204dd
fabian <godsonhm3@gmail.com>
2023-12-20 14:12:16 +0100

    fix bug in finding video by ID

--- controllers/video.js
+++ controllers/video.js
@@ -1,6 +1,5 @@
 const Video = require("../models/Video");
 const { StatusCodes } = require("http-status-codes");
-const checkPermissions = require("../utils/checkPermissions");
 const { BadRequestError, NotFoundError, CustomAPIError } = require("../errors");
 const cloudinary = require("cloudinary").v2;
 
@@ -19,7 +18,7 @@ const saveVideo = async (req, res) => {
 
   if (existingVideo) {
     const video = await Video.findByIdAndUpdate(
-      { publicId: req.body.publicId, user: req.user.userId },
+      { _id: existingVideo._id, user: req.user.userId },
       req.body,
       { new: true, runValidators: true }
     );
@@ -38,22 +37,30 @@ const getUserVideos = async (req, res) => {
 
 const getSingleVideo = async (req, res) => {
   const { id: videoId } = req.params;
-  const video = await Video.findOne({ _id: videoId });
+  const video = await Video.findOne({ _id: videoId, user: req.user.userId });
   if (!video) {
     throw NotFoundError(`No video with id : ${videoId}`);
   }
-  checkPermissions(req.user.userId, video.user);
   res.status(StatusCodes.OK).json({ video });
 };
 
 const deleteVideo = async (req, res) => {
   const {
     user: { userId },
-    params: { id: publicId },
+    params: { id: videoId },
   } = req;
 
+  const existingVideo = await Video.findOne({
+    _id: videoId,
+    user: userId,
+  });
+
+  if (!existingVideo) {
+    throw new NotFoundError(`No video with id ${videoId}`);
+  }
+
   cloudinary.uploader.destroy(
-    publicId,
+    existingVideo.publicId,
     {
       resource_type: "video",
     },
@@ -69,12 +76,9 @@ const deleteVideo = async (req, res) => {
   );
 
   const video = await Video.findByIdAndRemove({
-    publicId,
+    _id: existingVideo._id,
     userId,
   });
-  if (!video) {
-    throw new NotFoundError(`No video with id ${publicId}`);
-  }
 
   res.status(StatusCodes.OK).json("Video deleted from server");
 };
